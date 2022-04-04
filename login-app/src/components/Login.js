3cff2537c262b74dc69f8ee8357abf48dc6de9d8
Luciana Zanetti <luciana.zeta@gmail.com>
2022-04-04 13:12:34 -0300

    TailwindCSS

--- login-app/src/components/Login.js
+++ login-app/src/components/Login.js
@@ -1,7 +1,8 @@
 import { useState } from "react"
 import { useAuth } from '../context/authContext'
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import { Alert } from "./Alert";
+
 
 export const Login = () => {
     const [user, setUser] = useState({
@@ -46,22 +47,26 @@ const [error, setError] = useState()
     }
 
   return (
-    <div>
+    <div className= "w-full max-w-xs m-auto">
 
         {error && <Alert message={error}/> }
 
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+            
+            <div className="mb-4">
+                 <label htmlFor="email" className="block text-gray-700 text-sm font-fold mb-2">Email</label>
+                 <input type='email' name='email' placeholder="enter your email" onChange={handleChange} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus: outline-none focus:shadow-outline"/>
+            </div>
 
-            <label htmlFor="email">Email</label>
-            <input type='email' name='email' placeholder="enter your email" onChange={handleChange}/>
-
-            <label htmlFor="password">Password</label>
-            <input type='password' name='password' id= 'password'  placeholder="******" onChange={handleChange} />
-
-            <button>Login</button>
+            <div className="mb-4">
+                <label htmlFor="password">Password</label>
+                <input type='password' name='password' id= 'password'  placeholder="******" onChange={handleChange} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus: outline-none focus:shadow-outline" />
+            </div>
+            <button className="bg-blue-500 hover:bg-blue-700 text-white text-sm font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Login</button>
         </form>
+        <p className="my-4 text-sm flex justify-between px-3" >Don't have an Account? <Link to='/register'  className="text-blue-700 hover:text-blue-900"  ></Link>Register</p>
 
-        <button onClick={handleGoogleSignin}>Login with Google</button>
+        <button onClick={handleGoogleSignin}className="bg-slate-50 hover:bg-slate-200 text-black shadow-md rounded border-2 border-gray-300 py-2 px-4 w-full">Google Login</button>
     </div>
   )
 }
